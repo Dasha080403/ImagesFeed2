81fad07e1ba14d07af1bab65412b967540244bff
Dasha080403 <savinkina2003@mail.ru>
2026-01-06 17:53:15 +0300

    кнопка выхода и single image view

---
+++ Image Feed/Profile/ProfileController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileController.swift
+//  Image Feed
+//
+//  Created by Дарья Савинкина on 11.12.2025.
+//
+
+import UIKit
+
+final class ProfileController: UIViewController{
+    
+    @IBOutlet weak var exitButton: UIButton!
+    @IBOutlet weak var userPhoto: UIImageView!
+    @IBOutlet weak var informationLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+   
+    @IBAction func exitButtonDidTap(_ sender: Any) {
+    }
+}
